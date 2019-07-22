d769283ea186dc9509b33efc9577b671dfe10549
Afroz Quadri <46261706+afrozquadri@users.noreply.github.com>
2019-07-22 05:54:59 +0530

    Update vsphere.ps1

--- vsphere.ps1
+++ vsphere.ps1
@@ -6,7 +6,7 @@ add-pssnapin VMware.vimautomation.core
 }
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
 
-connect-viserver 192.168.1.171 -user administrator@cloud.local -password Abc@12345
+connect-viserver vc1.lab.local -user administrator@cloud.local -password Abc@12345
 
 get-vmhost
 
